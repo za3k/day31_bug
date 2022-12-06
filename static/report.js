0abac6f061e596f772579b71be4d6a0f595ef710
Zachary Vance <za3k@za3k.com>
2022-12-05 22:59:20 -0500

    Reduce functions; make opaque; fix systemd bug

--- static/report.js
+++ static/report.js
@@ -1,24 +1,5 @@
 'use strict';
 
-function randomId() {
-    return Math.floor(Math.random()*1000000000);
-}
-
-function getProject() {
-    let project = window.location.href.split("/")[3];
-    if (project!="hackaday") return project;
-    return `hack-a-${window.location.href.split("/")[4]}`
-}
-
-function getDate() {
-    const d = new Date()
-    return `${d.getUTCFullYear()}-${(d.getUTCMonth()+1).toString().padStart(2, '0')}-${d.getUTCDate().toString().padStart(2, '0')}`;
-}
-function getTime() {
-    const d = new Date()
-    return `${d.getUTCHours().toString().padStart(2, '0')}:${d.getUTCMinutes().toString().padStart(2, '0')}:${d.getUTCSeconds().toString().padStart(2, '0')}`;
-}
-
 function makeReporterStub() {
     document.body.insertAdjacentHTML("beforeend", `
         <div id="bug-reporter" style="position: fixed; bottom:0; right: 0; margin: 0; border: 2px solid; border-radius: 10px; padding: 4px; text-decoration: none; font-size: 18pt;">
@@ -31,12 +12,20 @@ function makeReporter() {
     const reporter = document.getElementById("bug-reporter");
     if (reporter) return;
 
-    const id = randomId();
+    const id = Math.floor(Math.random()*1000000000);
+
     const url = window.location.href;
-    const project = getProject();
-    const date = `${getDate()} ${getTime()} UTC`;
+
+    let project = window.location.href.split("/")[3];
+    if (project=="hackaday") project = `hack-a-${window.location.href.split("/")[4]}`
+
+    const d = new Date()
+    const date = `${d.getUTCFullYear()}-${(d.getUTCMonth()+1).toString().padStart(2, '0')}-${d.getUTCDate().toString().padStart(2, '0')}`;
+    const time = `${d.getUTCHours().toString().padStart(2, '0')}:${d.getUTCMinutes().toString().padStart(2, '0')}:${d.getUTCSeconds().toString().padStart(2, '0')}`;
+    const datetime = `${date} ${time} UTC`;
+
     document.body.insertAdjacentHTML("beforeend", `
-        <form method="POST" action="/hackaday/bug/report" id="bug-reporter" style="position: fixed; bottom:0; right: 0; border: 2px solid; margin: 0;" enctype="multipart/form-data">
+        <form method="POST" action="/hackaday/bug/report" id="bug-reporter" style="position: fixed; bottom:0; right: 0; border: 2px solid; margin: 0; z-index:99; background-color: white;" enctype="multipart/form-data">
         <table>
             <tr><td colspan="2">
                 Bug Reporter
@@ -47,8 +36,8 @@ function makeReporter() {
                 ${id}
             </td></tr>
             <tr><td>date, time</td><td>
-                <input type="hidden" name="date" value="${date}"/>
-                ${date}
+                <input type="hidden" name="date" value="${datetime}"/>
+                ${datetime}
             </td></tr>
             <tr><td>type</td><td>
                 <select name="type">
@@ -93,14 +82,10 @@ What actually happened:
     `);
 }
 
-function main() {
-    makeReporterStub();
-}
-
 (function() {
     function docReady(fn) { // https://stackoverflow.com/questions/9899372/vanilla-javascript-equivalent-of-jquerys-ready-how-to-call-a-function-whe. Avoiding jquery because it's messing up error display
         if (document.readyState === "complete" || document.readyState === "interactive") setTimeout(fn, 1);
         else document.addEventListener("DOMContentLoaded", fn);
     }
-    docReady(main);
+    docReady(makeReporterStub);
 })();
